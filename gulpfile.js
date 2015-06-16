9efe6c1f1ca1a6aa1c5402e15b0fefd87ff22275
tonylampada <tonylampada@gmail.com>
2015-06-16 01:12:57 -0300

    begginnig component catalog

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,59 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var linker = require('gulp-linker');
  
-gulp.task('concatjs', function() {
-    return gulp.src('./src/**/*.js')
-        .pipe(concat('fs.js'))
-        .pipe(gulp.dest('./dist/js/'));
-});
+var srcjs = [
+    './src/**/*.js',
+    '!./src/**/docs/**/*.js',
+];
+var docsjs = [
+    './src/**/docs/**/*.js',
+    '!./src/**/docs/**/test_*.js',
+];
+
+var libjs = [
+    './lib/angular-1.4.0/angular.js',
+    './lib/angular-1.4.0/angular-aria.js',
+    './lib/angular-1.4.0/angular-animate.js',
+    './lib/angular-material-0.9.8/angular-material.js',
+];
+var libjsmin = [
+    './lib/angular-1.4.0/angular.min.js',
+    './lib/angular-1.4.0/angular-aria.min.js',
+    './lib/angular-1.4.0/angular-animate.min.js',
+    './lib/angular-material-0.9.8/angular-material.min.js',
+];
+
+gulp.task('js', ['concatjssrc', 'concatjsdocs', 'concatjslib', 'concatjslibmin']);
+
+concattask('concatjssrc', srcjs, 'fs.js');
+concattask('concatjsdocs', docsjs, 'fs_docs.js');
+concattask('concatjslib', libjs, 'lib.js');
+concattask('concatjslibmin', libjsmin, 'lib.min.js');
+
 gulp.task('linkjs', function() {
     return gulp.src('./src/pages/*.html')
-        .pipe(linker({
-            scripts: [ './src/**/*.js' ],
-            startTag: '<!--JS-->',
-            endTag: '<!--JS END-->',
-            fileTmpl: '<script src="%s"></script>',
-            // appRoot: 'approot/'
-        }))
+        .pipe(linker(linker_params(srcjs, 'SRCJS')))
+        .pipe(linker(linker_params(docsjs, 'DOCSJS')))
         .pipe(gulp.dest('./dist/'));
 });
+
+function concattask(id, src, dest){
+    gulp.task(id, function() {
+        return gulp.src(src)
+            .pipe(concat(dest))
+            .pipe(gulp.dest('./dist/js/'));
+    });
+}
+
+function linker_params(src, marker){
+    return {
+        scripts: src,
+        startTag: '<!--'+marker+'-->',
+        endTag: '<!--'+marker+' END-->',
+        fileTmpl: '<script src="%s"></script>',
+        appRoot: '.'
+    };
+}
+
+
 
--- src/docs/component_catalog/component_catalog.js
+++ src/docs/component_catalog/component_catalog.js
@@ -5,40 +5,40 @@ if(!DOCS.angular_dependencies){
     DOCS.angular_dependencies = [];
 }
 angular.module('component_catalog', []);
-angular.module('component_catalog').factory('CatalogoComponentes', function(){
+angular.module('component_catalog').factory('ComponentCatalog', function(){
     var catalog = {
-        componentes: []
+        components: []
     };
 
     catalog.add_test_page = function(options){
-        options.js = !Array.isArray(options.js) && options.js !== undefined ? [options.js] : options.js;
-        options.testes = !Array.isArray(options.testes) && options.testes !== undefined ? [options.testes] : options.testes;
+        options.js = _2array(options.js);
+        options.tests = _2array(options.tests);
 
-        var componente = {
+        var testpage = {
             group: options.group,
             title: options.title,
             category: options.category,
             folder: options.folder,
             example: options.example,
             js: options.js,
-            testes: options.testes,
+            tests: options.tests,
             usage: options.usage,
             source: '',
             markdown: '',
             active: false,
         };
-        catalog.componentes.push(componente);
+        catalog.components.push(component);
     };
 
     catalog.set_active = function(index) {
-        catalog.componentes[index].active = true;
+        catalog.components[index].active = true;
     };
 
     catalog.get = function(category, title){
         var index;
-        var result = catalog.componentes.filter(function( componente, i ) {
-            if(componente.category == category &&
-               componente.title == title){
+        var result = catalog.components.filter(function( component, i ) {
+            if(component.category == category &&
+               component.title == title){
                 index = i;
                 return componente;
             }
@@ -49,123 +49,129 @@ angular.module('component_catalog').factory('CatalogoComponentes', function(){
         return result[0];
     };
 
+    function _2array(v){
+        return !Array.isArray(v) && v !== undefined ? [v] : v;
+    }
+
     return catalog;
 });
 
-angular.module('component_catalog').directive('catalogoComponentesTree', function(CatalogoComponentes){
+angular.module('component_catalog').factory('ComponentCatalogTreeModel', function(ComponentCatalog){
+    var m = {
+        active_component: null,
+        components: [],
+        categories: [],
+        init: init,
+        toggle_open: toggle_open,
+        activate: activate,
+    };
+
+    function init(group_filter){
+        m.components = ComponentCatalog.components.filter(function(e){
+            return e.group == group_filter;
+        });
+        var catmap = {}
+        m.categories = [];
+        for(var i=0; i<m.components.length; i++){
+            var category = m.components[i].category;
+            if(!catmap[category]){
+                m.categories.push({name: category});
+                catmap[category] = true;
+            }
+        }
+    }
+
+    function toggle_open(category){
+        category.open = !category.open;
+    }
+
+    function activate(component){
+        m.active_component = component;
+    }
+
+    return m;
+})
+
+angular.module('component_catalog').directive('componentCatalogTree', function(ComponentCatalogTreeModel){
     return {
         restrict: 'E',
         scope: {
             group: '@'
         },
-        templateUrl: EM.get_baseurl()+'js/estante/commons/docs/catalogo_componentes/catalogo_componentes_tree.html',
+        templateUrl: FS.BASE_URL+'/docs/component_catalog/component_catalog_tree.html',
         controller: function($scope) {
-            $scope.componentes = CatalogoComponentes.componentes.filter(function(e){
-                return e.group == $scope.group;
-            });
-
-            $scope.categories = [];
-
-            // retorna um array com as categorias não duplicadas
-            $.each(
-                // mapeia as categorias dos componentes
-                $scope.componentes.map(
-                    function(e){ return {name: e.category}; }
-                ),
-                // remove ocorrencias duplicadas
-                function(i, el){
-                    // Se não existe no objecto ele adiciona
-                    if(!($scope.categories.filter(function(e){ return e.name === el.name; } )).length){
-                        $scope.categories.push(el);
-                    }
-                }
-            );
-
-            $scope.toggle_category = function(category){
-                //close all categorys
-                // for (var i = 0; i < $scope.categories.length; i++) {
-                //     $scope.categories[i].open = false;
-                // };
-                category.open = !category.open;
-            };
-
-            $scope.toggle_active = function(componente){
-                //close all categorys
-                for (var i = 0; i < $scope.componentes.length; i++) {
-                    $scope.componentes[i].active = false;
-                }
-                componente.active = true;
-            };
+            var m = $scope.m = ComponentCatalogTreeModel;
+            m.init($scope.group);
         }
     };
 });
 
-angular.module('component_catalog').controller('IncludeComponentController', function($scope, $stateParams, $http, $location, CatalogoComponentes,LayoutModel){
+// angular.module('catalogo_componentes').controller('IncludeComponentController', function($scope, $stateParams, $http, $location, CatalogoComponentes,LayoutModel){
 
-    $scope.layout = LayoutModel;
+//     $scope.layout = LayoutModel;
 
-    $scope.get_example = function() {
-        var url = $scope.componente.folder + $scope.componente.example;
-        $http.get(url).then(function(result){
-            $scope.componente.source = result.data;
-        });
-    };
+//     $scope.get_example = function() {
+//         var url = $scope.componente.folder + $scope.componente.example;
+//         $http.get(url).then(function(result){
+//             $scope.componente.source = result.data;
+//         });
+//     };
 
-    $scope.get_source = function(files) {
-        var sources = [];
+//     $scope.get_source = function(files) {
+//         var sources = [];
 
-        angular.forEach(files, function(value, key){
-            var url = $scope.componente.folder + files[key];
-            $http.get(url).then(function(result){
-                sources[files[key]] = result.data;
-            });
-        });
+//         angular.forEach(files, function(value, key){
+//             var url = $scope.componente.folder + files[key];
+//             $http.get(url).then(function(result){
+//                 sources[files[key]] = result.data;
+//             });
+//         });
 
-        return sources;
-    };
+//         return sources;
+//     };
 
-    var category = $stateParams.category;
-    var title = $stateParams.title;
-    var componente = CatalogoComponentes.get(category, title);
-    $scope.componente = componente;
+//     var category = $stateParams.category;
+//     var title = $stateParams.title;
+//     var componente = CatalogoComponentes.get(category, title);
+//     $scope.componente = componente;
 
-    if($scope.componente.example){
-        $scope.get_example();
-    }
-    if($scope.componente.js){
-        $scope.componente.source_js = $scope.get_source($scope.componente.js);
-    }
+//     if($scope.componente.example){
+//         $scope.get_example();
+//     }
+//     if($scope.componente.js){
+//         $scope.componente.source_js = $scope.get_source($scope.componente.js);
+//     }
 
-    if($scope.componente.testes){
-        $scope.componente.source_teste = $scope.get_source($scope.componente.testes);
-    }
+//     if($scope.componente.testes){
+//         $scope.componente.source_teste = $scope.get_source($scope.componente.testes);
+//     }
 
-    // pega parametros da url para inicializar a tab
-    $scope.show_uso = $location.search().tab_view == 'uso' ? true : false;
-    $scope.show_exemplo = $location.search().tab_view == 'exemplo' ? true : false;
-    $scope.show_codigo = $location.search().tab_view == 'fonte' ? true : false;
-    $scope.show_js = $location.search().tab_view == 'js' ? $location.search().file : false;
-    $scope.show_teste = $location.search().tab_view == 'teste' ? $location.search().file : false;
+//     // pega parametros da url para inicializar a tab
+//     $scope.show_uso = $location.search().tab_view == 'uso' ? true : false;
+//     $scope.show_exemplo = $location.search().tab_view == 'exemplo' ? true : false;
+//     $scope.show_codigo = $location.search().tab_view == 'fonte' ? true : false;
+//     $scope.show_js = $location.search().tab_view == 'js' ? $location.search().file : false;
+//     $scope.show_teste = $location.search().tab_view == 'teste' ? $location.search().file : false;
 
-    if(!$location.search().tab_view){
-        $scope.show_exemplo = true;
-    }
+//     if(!$location.search().tab_view){
+//         $scope.show_exemplo = true;
+//     }
 
-    $scope.set_view = function(view, item){
-        $scope.show_uso = view == 'uso';
-        $scope.show_exemplo = view == 'exemplo';
-        $scope.show_codigo = view == 'fonte';
-        $scope.show_js = view == 'js' ? item : false;
-        $scope.show_teste = view == 'teste' ? item : false;
+//     $scope.set_view = function(view, item){
+//         $scope.show_uso = view == 'uso';
+//         $scope.show_exemplo = view == 'exemplo';
+//         $scope.show_codigo = view == 'fonte';
+//         $scope.show_js = view == 'js' ? item : false;
+//         $scope.show_teste = view == 'teste' ? item : false;
 
-        $location.search('tab_view', view);
+//         $location.search('tab_view', view);
 
-        if(item !== undefined){
-            $location.search('file', item);
-        }
-    };
-});
+//         if(item !== undefined){
+//             $location.search('file', item);
+//         }
+//     };
+// });
 
-angular.module('component_catalog').controller('developersCtrl', function($scope, LayoutModel){
-    $scope.layout = LayoutModel;
-});
+// angular.module('catalogo_componentes').controller('developersCtrl', function($scope, LayoutModel){
+//     $scope.layout = LayoutModel;
+// });
